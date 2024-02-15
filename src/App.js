890cc736c4f658a1851c947e2bdffac775d39e94
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-15 19:08:04 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './counter';
 
 function App() {
   return (
     <div className="App">
     <h1>Welcome to Reactapp</h1>
+    <Counter></Counter>
     </div>
   );
 }
