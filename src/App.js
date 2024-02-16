582b1d055475143e78a2950ea6efa8ec5af2af85
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-16 12:34:25 +0530

    second

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './counter';
 
+// function App() {
+//   return (
+//     <div className="App">
+//     <h1>Welcome to Reactapp</h1>
+//     <Counter></Counter>
+//     <Counter></Counter>
+//     <h1>Maha</h1>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
     <h1>Welcome to Reactapp</h1>
-    <Counter></Counter>
+    <Counter cname="india" cvaluestarts={10} cinc={5}></Counter>
+    <Counter cname="australia" cvaluestarts={200} cinc={12}></Counter>
     <h1>Maha</h1>
     </div>
   );
--- src/counter.jsx
+++ src/counter.jsx
@@ -1,17 +1,44 @@
 import React from "react"
-function Counter(){
-    var[c,setC]=React.useState(0)
+// function Counter(){
+//     var[c,setC]=React.useState(0)
+//         function inc(){
+//             setC(c+1)
+//         }
+        
+//     function dec(){
+//         setC(c-1)
+//     }
+  
+//   return(
+//        <div>
+//              <h1>Counter:{c}</h1>
+//              <button onClick={()=>(inc())}>Increment</button>
+//              <button onClick={()=>(dec())}>Decrement</button>
+
+//     </div>
+//   )
+// }
+
+
+function Counter(x){
+    var[c,setC]=React.useState(x.cvaluestarts)
+
         function inc(){
-            setC(c+1)
+            setC(c+x.cinc)
+    
         }
         
     function dec(){
-        setC(c-1)
+        setC(c-x.cinc)
+
     }
+
   
   return(
        <div>
-             <h1>Counter:{c}</h1>
+             <h1>{x.cname}:{c}</h1>
+        
+            
              <button onClick={()=>(inc())}>Increment</button>
              <button onClick={()=>(dec())}>Decrement</button>
 
