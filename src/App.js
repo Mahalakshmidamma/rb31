a1b3d95bdbfa3bb08173069b7e60f7e387d86086
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-22 16:59:28 +0530

    MMM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Products from "./Products"
+import React from 'react'
+import axios from 'axios'
 
 function App() {
   return (
-    <div className="App">
+    <div className="mybox">
     <h1>Welcome to Reactapp</h1>
     <Products></Products>
     </div>
--- src/Products.jsx
+++ src/Products.jsx
@@ -14,16 +14,17 @@ function Products(){
     return (
         <div>
             <h1>PRODUCTS</h1>
-            <div className="d-flex flex-wrap w-75">
+            <div className="d-flex flex-wrap w-75 ">
                  {
                 products?.map((product)=>{
                     return (
-                       <div class="card m-2" style={{width:"250px"}}>
-                         <img src={product.image} class="card-img-top" alt="..."/>
-                         <div class="card-body">
+                       <div class="card m-2" >
+                         <img src={product.image} class="card-img-top" style={{width:'250px'}} alt="..."/>
+                         <div class="card-body d-flex flex column justify-content-between">
                             <h5 class="card-title">{product.title}</h5>
                             <p class="card-text">{product.description.slice(0,100)}</p>
                             <p class="card-text"><small class="text-body-secondary">{product.category}</small></p>
+                            <button className='btn btn-success'>Add to Cart</button>
                          </div>
                        </div>
                     
