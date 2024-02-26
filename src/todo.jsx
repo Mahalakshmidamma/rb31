b1db0f7039601724cfb328cb207593279d5736b8
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-26 15:54:02 +0530

    mmm

--- src/todo.jsx
+++ src/todo.jsx
@@ -3,20 +3,22 @@ import React from "react";
 function Todo({todo,index,del,done,undo,edit}){
     // console.log("Todo")
     return (
-        <div className="listDiv">
+        <div style={{backgroundColor:"red"}}>
+            <div className="listDiv">
             
-                  <li style={{textDecoration:todo.stats?"line-through":"none"}}>
-                
-                  {todo.task} 
+            <li id="hihi"  style={{textDecoration:todo.stats?"line-through":"none"}}>
+          
+            {todo.task} 
 
-                  <button className="btn btn-danger m-2" onClick={()=>(del(index))}>Delete</button>
-                  <button className="btn btn-success m-2" onClick={()=>(done(index))}>Done</button>
-                  <button className="btn btn-warning m-2" onClick={()=>(undo(index))}>Undo</button>
-                  <button className="btn btn-primary m-2" onClick={()=>(edit(index))}>Edit</button> 
+            <button className="btn btn-danger m-2 " onClick={()=>(del(index))}>Delete</button>
+            <button className="btn btn-success m-2" onClick={()=>(done(index))}>Done</button>
+            <button className="btn btn-warning m-2" onClick={()=>(undo(index))}>Undo</button>
+            <button className="btn btn-primary m-2" onClick={()=>(edit(index))}>Edit</button> 
 
-          </li>
-            
+    </li>
+      
 
+  </div>
         </div>
     )
 }
