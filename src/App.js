d372eec1df2dc4d425a798b4aa3c198cda38b10e
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-27 16:15:54 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useFormik } from 'formik';
+
 
 function App() {
+  var userForm=useFormik({
+    initialValues:{"fn":""},
+    onSubmit:()=>{}
+  })
   return (
     <div className="App">
-    
+    <h1>Form Handling</h1>
     </div>
   );
 }
