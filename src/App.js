96719a81d2df7d5de4bb3377118f4e066a93a88a
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-27 17:33:33 +0530

    mm

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import './App.css';
 import { useFormik } from 'formik';
 
 
+
 function App() {
-  var userForm=useFormik({
+  var userForm =useFormik({
     initialValues:{"fn":"","ln":"","age":""},
     onSubmit:(fv)=>{console.log(fv)}
   })
+  console.log(userForm)
   return (
-    <div className="App">
+    <div className="">
     <h1>Form Handling</h1>
+    {/* <form onSubmit={userForm.handleSubmit}>
+    <input type="text" name="fn" placeholder="Firstname" onChange={}/><br/>
+    <input type="text" name="ln" placeholder="Lastname" onChange={}/><br/>
+    <input type="text" name="age" placeholder="Age" onChange={}/><br/>
+    <button type="submit">Save</button>
+    </form> */}
     </div>
   );
 }
