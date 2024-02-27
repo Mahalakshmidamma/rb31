ff087334343cbd699b5e3e35e3e5eab38aec0412
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-02-27 18:39:41 +0530

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import './App.css';
-import { useFormik } from 'formik';
+// import './App.css';
+// import { useFormik } from 'formik';
 
 
 
-function App() {
-  var userForm =useFormik({
-    initialValues:{"fn":"","ln":"","age":""},
-    onSubmit:(fv)=>{console.log(fv)}
-  })
-  console.log(userForm)
+// function App() {
+//   var userForm =useFormik({
+//     initialValues:{"fn":"","ln":"","age":""},
+//     onSubmit:(fv)=>{console.log(fv)}
+//   })
+//   console.log(userForm)
+//   return (
+//     <div className="">
+//     <h1>Form Handling</h1>
+//     <form onSubmit={userForm.handleSubmit}>
+//     <input type="text" name="fn" placeholder="Firstname" onChange={userForm.handleChange}/><br/>
+//     <input type="text" name="ln" placeholder="Lastname" onChange={userForm.handleChange}/><br/>
+//     <input type="text" name="age" placeholder="Age" onChange={userForm.handleChange}/><br/>
+//     <button type="submit">Save</button>
+//     </form>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import './App.css'
+import { Formik } from 'formik'
+function App(){
   return (
-    <div className="">
-    <h1>Form Handling</h1>
-    {/* <form onSubmit={userForm.handleSubmit}>
-    <input type="text" name="fn" placeholder="Firstname" onChange={}/><br/>
-    <input type="text" name="ln" placeholder="Lastname" onChange={}/><br/>
-    <input type="text" name="age" placeholder="Age" onChange={}/><br/>
-    <button type="submit">Save</button>
-    </form> */}
-    </div>
-  );
+    <>
+    <h1>welcome to formik </h1>
+    <Formik
+        initialValues={{"fn":"","ln":"","age":""}}
+        onSubmit={(fv)=>{console.log(fv)}}
+        >
+        {
+         ({handleSubmit,handleChange})=>{
+          // console.log(userForm)
+          return (
+            <form onSubmit={handleSubmit}>
+              <input type="text" placeholder="Firstname" name="fn" onChange={handleChange}/><br/>
+              <input type="text" placeholder="Lastname" name="ln" onChange={handleChange}/><br/>
+              <input type="text" placeholder="Age" name="age" onChange={handleChange}/><br/>
+              <button type="submit">Save</button>
+            </form>
+          )
+         }
+        }
+    </Formik>
+    </>
+  )
 }
-
 export default App;
