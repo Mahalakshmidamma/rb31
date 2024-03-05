8efb0f8df195e8cc56533e5e382b502ab8ff886e
Mahalakshmidamma <mahalakshmidamma714@gmail.com>
2024-03-05 18:27:43 +0530

    second

---
+++ src/navbar.jsx
@@ -0,0 +1,15 @@
+
+function Navbar(){
+    return(
+        
+        <nav class="navbar bg-body-tertiary">
+  <div class="container">
+  <button id="btn1">All Enquiries</button>
+    <a class="navbar-brand" href="#">
+      <img src="https://edupoly.in/common/assets/edupoly-logo-light.png" alt="Bootstrap" width="100" height="30"/>
+    </a>
+  </div>
+</nav>
+    )
+}
+export default Navbar
